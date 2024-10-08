58e6f665a5a0c282eb1dd7267900e23cb4356c20
berkagmpp <berkagmpp@gmail.com>
2024-10-08 15:47:24 +1300

    FEAT add removal btn on li with prentNode.remove()

--- 2_dom-control/2_dynamic-element.js
+++ 2_dom-control/2_dynamic-element.js
@@ -1,12 +1,19 @@
 let inputBox = document.querySelector("input");
-
 // Add dynamic li element node
 
 // 1. appendChild()
 document.getElementById("button").addEventListener("click", function () {
     let ui = document.querySelector("ul");
     let li = document.createElement("li");
+    let remove = document.createElement("button");
+    let removeMark = document.createTextNode("x");
+    remove.setAttribute("class", "targetRemoveBtn");
+    remove.addEventListener("click", function (event) {
+        event.target.parentNode.remove();
+    });
     li.textContent = inputBox.value;
+    remove.appendChild(removeMark);
+    li.appendChild(remove);
     ui.appendChild(li);
     // appendChild() method adds a node to the end of the list
 });
@@ -21,3 +28,10 @@ document.getElementById("before").addEventListener("click", function () {
     // insertBefore() method inserts a node before a reference node
     // as a child of a specified parent node
 });
+
+// 3. remove
+document
+    .querySelector(".targetRemoveBtn")
+    .addEventListener("click", function (event) {
+        event.target.parentNode.remove();
+    });
