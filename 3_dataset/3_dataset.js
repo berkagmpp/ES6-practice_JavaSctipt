97315bafb57532930a9d1bfabc10b9afee668a33
berkagmpp <berkagmpp@gmail.com>
2024-10-09 09:49:16 +1300

    Feat: Add dynamic text content text change when user click the button use event.target textContent
    Resolves: #3002

--- 3_dataset/3_dataset.js
+++ 3_dataset/3_dataset.js
@@ -4,16 +4,25 @@ console.log(h2.dataset.test);
 
 let liList = document.querySelectorAll("li");
 let img = document.querySelector("img");
+let selectedItem = document.querySelector(".selected-item");
 
 liList[0].addEventListener("click", function (event) {
     img.setAttribute("src", event.target.dataset.img);
+    selectedItem.textContent = event.target.textContent;
 });
 
 liList[1].addEventListener("click", function (event) {
     img.setAttribute("src", event.target.dataset.img);
+    selectedItem.textContent = event.target.textContent;
 });
 
 liList[2].addEventListener("click", function (event) {
     // console.log(event.target.dataset.img);
     img.setAttribute("src", event.target.dataset.img);
+    selectedItem.textContent = event.target.textContent;
 });
+
+const btn = document.querySelector("[data-tooltip]");
+
+btn.dataset.tooltip = "This is a tooltip";
+console.log(btn.dataset.tooltip);
