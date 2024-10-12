69eb3bd7fbe634897692492ccead2b931b0f308e
berkagmpp <berkagmpp@gmail.com>
2024-10-12 14:18:35 +1300

    Feat: conditional - switch
    resolve: #7005

--- 7_js-basic/conditional.js
+++ 7_js-basic/conditional.js
@@ -46,3 +46,39 @@ if (value7) {
 } else {
     console.log('value3 is false');
 }
+
+// conditionals -switch
+const fruit = 'apple';
+switch (fruit) {
+    case 'banana':
+        console.log('This is a banana');
+        break; // quit the switch statement after the case is executed.
+    case 'apple':
+        console.log('This is an apple');
+        break; // if break is not included, the next case
+    case 'orange':
+        console.log('This is an orange');
+        break;
+    default: // like else in if statement
+        console.log('I do not know what fruit this is');
+}
+
+const score2 = prompt('Enter your score: ');
+const value = Math.floor(score2 / 10);
+switch (value) {
+    case 10:
+    case 9: // many cases can be assigned to the same code block
+        alert('your grade is A');
+        break;
+    case 8:
+        alert('your grade is B');
+        break;
+    case 7:
+        alert('your grade is C');
+        break;
+    case 6:
+        alert('your grade is D');
+        break;
+    default:
+        alert('your grade is F');
+}
