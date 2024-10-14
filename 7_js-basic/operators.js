3657496da850602c8da77616b098988f8ee8fea7
berkagmpp <berkagmpp@gmail.com>
2024-10-14 17:15:00 +1300

    Feat: operator - comparison, assignment
    resolve: #7010

--- 7_js-basic/operators.js
+++ 7_js-basic/operators.js
@@ -37,3 +37,37 @@ let result2 = a++ + b;
 console.log('a++ + b:', result2); // 15
 // result2 is 15, because after the operation, the increment is executed like below
 console.log('a:', a, 'b:', b); // a: 11 b: 5
+
+// comparison operators
+// always return boolean value --> true or false
+const value1 = 10;
+const value2 = 15;
+console.log(value1 < value2); // true
+console.log(value1 >= value2); // false
+
+const value3 = 10;
+const value4 = '10';
+console.log(value3 == value4); // true
+// ==: loose equality operator, the same value but different types
+// type coercion: JavaScript automatically converts the data type from string('10) to number(10)
+console.log(value3 === value4); // false
+// ===: strict equality operator, the same value and the same type
+// always use === to avoid bugs
+console.log(value3 !== value4); // true
+// !==: strict inequality operator
+console.log(value3 != value4); // false
+// !=: loose inequality operator
+// != -> convert the string to number, so value3 and value4 are the same
+
+// === and !== are recommended to use
+// == and != are not recommended to use
+
+// assignment operators
+let k = 10;
+const l = 5;
+
+k = 4; // =: assignment operator
+console.log(k); // 4
+
+k += l; // k = k + l
+console.log(k); // 9
