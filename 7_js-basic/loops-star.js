542f59395bfe3ce0caf2cb5f5cac1eaf1b3aba23
berkagmpp <berkagmpp@gmail.com>
2024-10-14 16:29:04 +1300

    Feat: practice drawing shapes using for loop 2
    resolve: #7008

--- 7_js-basic/loops-star.js
+++ 7_js-basic/loops-star.js
@@ -1,6 +1,11 @@
 const content = document.getElementById('content');
 let stars = '';
 
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
 for (let i = 0; i < 5; i++) {
     for (let j = 0; j < 5; j++) {
         stars += '*';
@@ -9,6 +14,11 @@ for (let i = 0; i < 5; i++) {
 }
 content.textContent = stars;
 
+// *
+// **
+// ***
+// ****
+// *****
 for (let i = 0; i < 5; i++) {
     for (let j = 0; j < i + 1; j++) {
         stars += '*';
@@ -17,8 +27,79 @@ for (let i = 0; i < 5; i++) {
 }
 content.textContent = stars;
 
+// *****
+// ****
+// ***
+// **
+// *
 for (let i = 0; i < 5; i++) {
     for (let j = 5; j > i; j--) {
+        //for (let j = 0; j < 5 - i; j++) {     --> this is the same as the above line
+        stars += '*';
+    }
+    stars += '\r\n';
+}
+content.textContent = stars;
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+for (let i = 0; i < 5; i++) {
+    for (let j = 4; j > i; j--) {
+        stars += ' ';
+    }
+    for (let k = 0; k < i + 1; k++) {
+        stars += '*';
+    }
+    stars += '\r\n';
+}
+content.textContent = stars;
+
+// *****
+//  ****
+//   ***
+//    **
+//     *
+for (let i = 0; i < 5; i++) {
+    for (let k = 0; k < i; k++) {
+        stars += ' ';
+    }
+    for (let j = 5; j > i; j--) {
+        stars += '*';
+    }
+    stars += '\r\n';
+}
+content.textContent = stars;
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+for (let i = 0; i < 5; i++) {
+    for (let j = 4; j > i; j--) {
+        stars += ' ';
+    }
+    for (let k = 0; k < i * 2 + 1; k++) {
+        // i * 2 + 1 is the formula for the number of stars
+        stars += '*';
+    }
+    stars += '\r\n';
+}
+content.textContent = stars;
+
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+for (let i = 0; i < 5; i++) {
+    for (let k = 0; k < i; k++) {
+        stars += ' ';
+    }
+    for (let j = 9; j > i * 2; j--) {
         stars += '*';
     }
     stars += '\r\n';
