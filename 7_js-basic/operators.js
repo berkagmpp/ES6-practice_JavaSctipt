b4be4cc5c07528aa2d62e39ec4c5c070016b1279
berkagmpp <berkagmpp@gmail.com>
2024-10-14 16:56:46 +1300

    Feat: operator - string, number, increment and decrement (pre and post)
    resolve: #7009

---
+++ 7_js-basic/operators.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// string operators
+console.log('hello' + 5); // string + number = string
+console.log(typeof ('4' + 5)); // string
+console.log('3' + 5 + 6); // 356 -> string
+console.log(3 + 5 + '6'); // 86 -> string
+
+// number operators
+const num1 = 3;
+const num2 = 2;
+console.log(num1 + num2); // 5
+console.log(num1 - num2); //
+console.log(num1 * num2); // 6
+console.log(num1 / num2); // 1.5
+console.log(num1 % num2); // 1 -> remainder
+console.log(num1 ** num2); // 9 -> exponentiation
+
+// increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter; // after the increment, the operation is executed
+const postIncrement = counter++; // after the operation, the increment is executed
+const preDecrement = --counter;
+const postDecrement = counter--;
+// preIncrement: 3, postIncrement: 3, preDecrement: 2, postDecrement: 2
+// data type can be changed by increment and decrement operators
+
+let x = 10;
+let y = 5;
+console.log('x:', x, 'y:', y);
+let result = ++x + y;
+console.log('++x + y:', result); // 16
+console.log('x:', x, 'y:', y); // x: 11 y: 5
+let a = 10;
+let b = 5;
+let result2 = a++ + b;
+console.log('a++ + b:', result2); // 15
+// result2 is 15, because after the operation, the increment is executed like below
+console.log('a:', a, 'b:', b); // a: 11 b: 5
