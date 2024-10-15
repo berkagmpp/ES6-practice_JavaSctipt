607d88709086285a5ca6ac10557c90cd07a6a722
berkagmpp <berkagmpp@gmail.com>
2024-10-15 18:20:24 +1300

    Feat: object - conversion, creat, destructuring, factory conversion: key(), value(), entries(), JSON.stringify(), new Map(Object.entries()), Array.from(), fromEntries() creat: object literal and constructor sytex destructuring: general and assignment (assign object properties to multiple variables, function parameters get object properties factory: a funtion that returns an object, abbreviation of object properties(ES6)

--- 7_js-basic/object.js
+++ 7_js-basic/object.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // 1. object definition and properties
-// object is a collection of properties
+// 1.1) object is a collection of properties
 // object can contain multiple data types, other objects, functions (method), and arrays
 // properties are key-value pairs: key is a string (unique), value can be any data type
 
@@ -9,7 +9,12 @@ const person = {
     // defined by curly braces {}
     name: 'John', // key: value - property
     age: 30,
-    city: 'New York',
+    city: ['New York', 'Boston'], // array
+    hobby: {
+        //
+        name: 'reading',
+        level: 'beginner',
+    },
     sayHello: function () {
         // this is an object methods
         // define method look like this
@@ -20,86 +25,162 @@ const person = {
         console.log('Bye');
     },
 };
-// object properties can be accessed by dot notation (.) or bracket notation ([])
+// 1.2) object properties can be accessed by dot notation (.) or bracket notation ([])
 console.log(person.name); // John
 console.log(person['age']); // 30
+console.log(person.city[0]); // New York
+console.log(person.hobby.name); // reading
+console.log(person['hobby']['level']); // beginner
+console.log(person.hobby['name']); // reading    can mix dot and bracket notation
+console.log(person['hobby'].level); // beginner  can mix dot and bracket notation
 
-// object is mutable (can be changed)
+// 1.3) object is mutable (can be changed)
 person.name = 'Jane'; // update property
 console.log(person.name); // Jane
 console.log(typeof person); // object
 
-// object can be copied by reference (shallow copy)
+// 1.4) object can be copied by reference (shallow copy)
 const person2 = person; // copy by reference
 person2.name = 'Mike';
 console.log(person.name); // Mike
 
-// object can be copied by value (deep copy)
+// 1.5) object can be copied by value (deep copy)
 const person3 = { ...person }; // copy by value
 person3.name = 'Alice';
 console.log(person.name); // Mike
 console.log(person3.name); // Alice
 
-// object is created by using object literal or constructor
-const person4 = new Object(); // object constructor
-person4.name = 'Bob';
-console.log(person4.name); // Bob
-
-// object properties can be added, updated, and deleted
+// 1.6) object properties can be added, updated, and deleted
 delete person4.age; // delete property
 console.log(person4.age); // undefined
 
-// object methods are functions defined in an object
+// 1.7) object methods are functions defined in an object
 person.sayHello(); // Hello
 person.sayBye(); // Bye
 
-// object methods can be added, updated, and deleted
+// 1.8) object methods can be added, updated, and deleted
 person.sayHi = function (name) {
     console.log(`Hi ${name}`);
 };
 person.sayHi(); // Hi undefined
 person.sayHi('Tom'); // Hi Tom
 
-// object methods can access object properties using 'this' keyword
+// 1.9) object methods can access object properties using 'this' keyword
 person.sayHello = function () {
     console.log(`Hello ${this.name}`);
 };
 person.sayHello(); // Hello Mike
 
-// object can be destructured
-const { city, age } = person; // destructuring
-console.log(city); // New York
-console.log(age); // 30
-
-// object can be iterated by for in loop
+// 1.10) object can be iterated by for in loop
 for (let key in person) {
     console.log(key, person[key]);
 }
 // name Mike
 // age 30
-// city New York
+// city New York, Boston
 // sayHello [Function: sayHello]
 // sayBye [Function: sayBye]
 // sayHi [Function: sayHi]
 
 // 2. object conversion
-// object can be converted to an array by Object.keys(), Object.values(), Object.entries()
-// object can be converted to JSON string by JSON.stringify()
-// object can be converted to a map by Object.entries() and new Map()
-// object can be converted to an array by Array.from()
-// object can be converted to an object by Object.fromEntries()
-// object can be converted to a string by Object.toString()
-// object can be converted to a number by Number()
-// object can be converted to a boolean by Boolean()
-// object can be converted to a symbol by Symbol()
-// object can be converted to a date by new Date()
-// object can be converted to a function by Function()
-// object can be converted to a regular expression by RegExp()
-// object can be converted to a promise by Promise.resolve()
-// object can be converted to a set by Object.keys() and new Set()
-// object can be converted to a map by Object.keys() and new Map()
-// object can be converted to an array buffer by ArrayBuffer()
-// object can be converted to a data view by DataView()
 
-// create object
-// 1)
+const player = {
+    name: 'plyer1',
+    score: 0,
+    level: 1,
+    uplevel() {
+        this.level++;
+    },
+};
+// 2.1) object can be converted to an array by Object.keys(), Object.values(), Object.entries()
+const playerKeys = Object.keys(player); // get keys of the object
+console.log(playerKeys); // ['name', 'score', 'level']
+const playerValues = Object.values(player); // get values of the object
+console.log(playerValues); // ['player1', 0, 1]
+const playerEntries = Object.entries(player); // get key-value pairs of the object
+console.log(playerEntries); // [['name', 'player1'], ['score', 0], ['level', 1]]
+
+// 2.2) object can be converted to JSON string by JSON.stringify()
+const playerJSON = JSON.stringify(player);
+console.log(playerJSON); // {"name":"player1","score":0,"level":1}
+
+// 2.3) object can be converted to a map by Object.entries() and new Map()'
+const playerMap = new Map(Object.entries(player));
+console.log(playerMap); // Map { 'name' => 'player1', 'score' => 0, 'level' => 1 }
+
+// 2.4) object can be converted to an array by Array.from()
+const playerArray = Array.from(player);
+console.log(playerArray); // ['player1', 0, 1]
+
+// 2.5) object can be converted to an object by Object.fromEntries()
+const playerObject = Object.fromEntries(playerEntries);
+console.log(playerObject); // { name: 'player1', score: 0, level: 1 }
+
+// 3. create object
+// 3.1) object literal syntax
+// define object by using curly braces {}
+// key: value pairs are separated by ',' inside {}
+const obj = {
+    key1: 'value1',
+    key2: 'value2',
+};
+
+console.log(obj); // { key1: 'value1', key2: 'value2' }
+
+// 3.2) object constructor syntax
+// define object by using new Object(): new is an operator and Object is a constructor function
+const obj2 = new Object();
+obj2.key1 = 'value1';
+obj2.key2 = 'value2';
+
+console.log(obj2); // { key1: 'value1', key2: 'value2' }
+
+// 4. destructuring object
+const bag = {
+    color: 'red',
+    size: 'medium',
+    weight: 2,
+};
+// 4.1) general destructuring
+const color = bag.color;
+const size = bag.size;
+const weight = bag.weight;
+
+// 4.2) destructuring assignment: assign object properties to multiple variables
+const { color2, size2, weight2 } = bag; // destructuring assignment
+console.log(color2, size2, weight2); // red medium 2
+
+// 4.3) destructuring assignment: function parameters get object properties
+function bagInfo({ color, size, weight }) {
+    console.log(`color: ${color}, size: ${size}, weight: ${weight}`);
+}
+bagInfo(bag); // color: red, size: medium, weight: 2
+
+// 5. factory function
+// factory function is a function that returns an object
+function createBag(color, size, weight) {
+    return {
+        // return an object like factory
+        color,
+        size,
+        weight,
+    };
+}
+const bag2 = createBag('blue', 'small', 1);
+const bag3 = createBag('green', 'large', 3);
+const bag4 = createBag('yellow', 'medium', 2);
+console.log(bag2); // { color: 'blue', size: 'small', weight: 1 }
+console.log(bag3); // { color: 'green', size: 'large', weight: 3 }
+console.log(bag4); // { color: 'yellow', size: 'medium', weight: 2 }
+
+// abbreviation of the object property (ES6)
+const kind = 'fruit';
+const taste = 'sweet';
+const price = 3;
+function createFruit(kind, taste, price) {
+    return {
+        kind, // kind: kind -> abbreviation (when the key and value are the same)
+        taste, // taste: taste
+        price, // price: price
+    };
+}
