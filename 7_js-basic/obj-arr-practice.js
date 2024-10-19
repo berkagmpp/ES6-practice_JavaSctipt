10b8c49be2f4bc629bf71321b9a135ca1bdaa161
berkagmpp <berkagmpp@gmail.com>
2024-10-19 15:56:11 +1300

    Feat: object and array practice2 map(), spread operator and destrucuring

--- 7_js-basic/obj-arr-practice.js
+++ 7_js-basic/obj-arr-practice.js
@@ -1,9 +1,9 @@
 'use strict';
 // objects and arrays
 
-// 1. map and spread operator
+// 1. map() and spread operator
 // change all the names to 'Park' in the array below.
-let students = [
+const students = [
     { id: 1, name: 'Kim', score: { math: 50, english: 70 } },
     { id: 2, name: 'Kim', score: { math: 80, english: 60 } },
     { id: 3, name: 'Lee', score: { math: 70, english: 50 } },
@@ -16,9 +16,9 @@ const newStudents = students.map((students) => ({ ...students, name: 'park' }));
 // By wrapping the object in parentheses, you make it clear to JavaScript that you intend to return an object, not define a function body.
 console.log(newStudents);
 
-// 2. filter, map, and spread operator
+// 2. filter(), map(), and spread operator
 // Find the people with the name 'Kim' in the array below, and add science: 100 and history: 100 to their score
-let students2 = [
+const students2 = [
     {
         id: 1,
         name: 'Kim',
@@ -37,11 +37,27 @@ let students2 = [
 ];
 const newStudents2 = students2
     .filter((item) => item.name === 'Kim')
+    // filter() -> filter the array. it returns a new array. can use only array.
     .map((students2) => ({
         ...students2,
         score: { ...students2.score, science: 100, history: 100 },
     }));
-// filter() -> filter the array. it returns a new array. can use only array.
 // in map(), spread(...students2) -> copy the value of students2 array.
 
 console.log(newStudents2);
+
+// 3. map(), the spread operator and destructuring
+// add a sum: (total score) property to the score object for each student in the array
+const students3 = [
+    { id: 1, name: 'Kim', score: { math: 50, english: 70 } },
+    { id: 2, name: 'Park', score: { math: 80, english: 60 } },
+    { id: 3, name: 'Lee', score: { math: 70, english: 50 } },
+];
+
+let newStudents3 = students3.map((students3) => {
+    const { math, english } = students3.score;
+    // destructuring -> extract the value of math and english from students3.score.
+    return { ...students3, score: { ...students3.score, sum: math + english } };
+    // return the object with the value of sum: math + english.
+});
+console.log(newStudents3);
